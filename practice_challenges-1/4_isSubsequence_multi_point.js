e2cf9e49f9734bc31782d59b361178f300a03f44
lukelin1991 <lukelin1991@yahoo.com>
2020-07-30 12:00:36 -0400

    isSubsequence algorithm multi point

--- practice_challenges-1/4_isSubsequence_multi_point.js
+++ practice_challenges-1/4_isSubsequence_multi_point.js
@@ -7,5 +7,14 @@ string appear somewhere in the second string, WITHOUT THEIR ORDER CHANGING.
 */
 
 function isSubsequence(str1, str2) {
+    let i = 0;
+    let j = 0;
+    if(!str1) return true
 
+    while(j < str2.length){
+        if(str2[j] === str1[i]) i++ // if an element in str2 matches w/ element in str1. move the 1st string slot up i++.
+        if(i === str1.length) return true // when i finishes going through the first string, you're good. 
+        j++; // always moving through the 2nd string.
+    }
+    return false;
 }
