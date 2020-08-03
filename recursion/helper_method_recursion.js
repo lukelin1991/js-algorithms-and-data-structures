da4579c5f4cb6d23e426d9a28b0e791e6210552e
lukelin1991 <lukelin1991@yahoo.com>
2020-08-03 13:38:20 -0400

    helper functions

---
+++ recursion/helper_method_recursion.js
@@ -0,0 +1,11 @@
+function outer(input){
+    let outerScopedVariable = []
+
+    function helper(helperInput){
+        // modify the outerScopedVariable
+        helper(helperInput--)
+    }
+    helper(input)
+
+    return outerScopedVariable
+}
