5b6d75805bcaba27b2191263d6698c24b3b15b33
lukelin1991 <lukelin1991@yahoo.com>
2020-08-03 14:33:23 -0400

    factorial recursion, exercise

--- practice_challenges-2/1_power_recursion.js
+++ practice_challenges-2/1_power_recursion.js
@@ -1,3 +1,10 @@
+/*
+Write a function called power which accepts a base and an exponent.
+The function should return the power of the base to the exponent.
+This function should mimic the functionality of Math.pow() 
+- do not worry about negative bases and exponents.
+*/
+
 function power(base, exp){
     if(exp === 0) return 1 // base case (to break out of loop)
     return base * power(base, exp - 1) // the recursion loop.
