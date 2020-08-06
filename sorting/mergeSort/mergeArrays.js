fd4e719549d77169f63e41620baa9b31f8ee97c7
lukelin1991 <lukelin1991@yahoo.com>
2020-08-06 15:22:20 -0400

    merging arrays, trying

---
+++ sorting/mergeSort/mergeArrays.js
@@ -0,0 +1,35 @@
+/*
+Merging Arrays
+in order to implement merge sort, it's useful to first implement a function responsible 
+for merging 2 sorted arrays.
+Given 2 arrays which are sorted, this helper function should create a new array 
+which is also sorted, and consists of all of the elements in the two input arrays.
+should run in O(n + m) time and O(n + m) space and should not modify the params passed to it.
+
+Pseudocode:
+create an empty array, take a look at the smallest values in each input array.
+have 2 numbers, i, j start at 0.
+while there are still values we haven't looked at,
+    if the value in the 1st array is smaller than the value in the 2nd array, push the value
+    in the 1st array into our results and move onto the next value in the 1st array. 
+
+    if the value in the 1st array is larger than the value in the 2nd array, push the value
+    in the 2nd array into our results and move onto the next value in the 2nd array.
+    Once we exhaust one array, push in all remaining values from the other array.
+*/
+
+function merge(arr1, arr2){
+    let result = []
+    let i = 0
+    let j = 0
+    while(i < arr1.length && j < arr2.length){
+        if(arr1[i] < arr2[j]){
+            result.push(arr1[i])
+            i++
+        } else if(arr1[i] > arr2[j]){
+            result.push(arr2[j])
+            j++
+        }
+    }
+
+}
