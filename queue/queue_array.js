5ee5a9b2e1de8dd68a2e4a2e505a92b94cd17fdf
lukelin1991 <lukelin1991@yahoo.com>
2020-08-14 15:18:42 -0400

    understanding Stacks and queues

---
+++ queue/queue_array.js
@@ -0,0 +1,8 @@
+let q = [];
+// adds on last element, removing 1st element, but re-indexing.
+q.push("hello");
+q.shift();
+
+//adds on first element, removing last element, no re-indexing.
+q.unshift("hello");
+q.pop();
