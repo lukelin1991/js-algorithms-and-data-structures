c37e1ab5dc7df06d605102319863e377e3f9ef35
lukelin1991 <lukelin1991@yahoo.com>
2020-08-17 17:19:57 -0400

    inorder done

--- Tree_traversal/DFS_tree.js
+++ Tree_traversal/DFS_tree.js
@@ -18,6 +18,15 @@ write a helper function which accepts a node
     - push the value of the node to the variable that stores the values
     - invoke the helper function with the current variable.
 return the array of values
+
+DFS - InOrder
+create a variable to store the values of nodes visited
+store the root of the BST in a variable called current
+write a helper function which accepts a node
+    - if the node has a left property, call the helper function with the left property on the node
+    - push the value of the node to the variable that stores the values
+    - if the node has a right property, call the helper function with the right property on the node
+Invoke the helper function with the current variable.
 */
 
 class Node {
@@ -97,7 +106,7 @@ class BinarySearchTree {
         queue.push(this.root)
         while(queue.length){
             node = queue.shift()
-            data.push(node) // use node.value to find the element/value of the node.
+            data.push(node.value) // use node.value to find the element/value of the node.
             if(node.left) queue.push(node.left)
             if(node.right) queue.push(node.right)
         }
@@ -121,6 +130,17 @@ class BinarySearchTree {
             if(node.left) traverse(node.left)
             if(node.right) traverse(node.right)
             data.push(node.value)
+        }
+        traverse(this.root)
+        return data
+    }
+    DFSInOrder(){
+        let data = []
+
+        function traverse(node){
+            if(node.left) traverse(node.left)
+            data.push(node.value)
+            if(node.right) traverse(node.right)
         }
         traverse(this.root)
         return data
