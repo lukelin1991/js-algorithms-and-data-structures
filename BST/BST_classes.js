fa1594ddc910e7cc02d1425717e18b1f3010dffa
lukelin1991 <lukelin1991@yahoo.com>
2020-08-17 13:09:05 -0400

    BST inserting

---
+++ BST/BST_classes.js
@@ -0,0 +1,61 @@
+/*
+inserting a node
+create a new node
+starting at the root
+    - check if there is a root, if not - the root now becomes that new node!
+    - if there is a root, check if the value of the new node is greater than or less than the value of the root
+    - if it is greater
+        - Check to see if there is a node to the right
+            - if there is, move to that node & repeat these steps
+            - if there is not, add that node as the right property
+    - if it is less
+        - Check to see if there is a node to the left
+            - if there is, move to that node and repeat these steps
+            - if there is not, add that node as the left property
+*/
+
+class Node {
+    constructor(value){
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+class BinarySearchTree {
+    constructor(){
+        this.root = null
+    }
+    insert(value){
+        let newNode = new Node(value)
+        if(this.root === null){
+            this.root = newNode
+            return this
+        }
+        let current = this.root
+        while(true){
+            if(value === current.value) return undefined;
+
+            if(value < current.value){
+                if(current.left === null){
+                    current.left = newNode
+                    return this
+                }
+                current = current.left
+
+            } else if(value > current.value){
+                if(current.right === null){
+                    current.right = newNode
+                    return this
+                }
+                current = current.right
+            }
+        }
+    }
+}
+
+let tree = new BinarySearchTree();
+tree.root = new Node(10)
+tree.root.right = new Node(15)
+tree.root.left = new Node(7)
+this.root.left.right = new Node(9)
