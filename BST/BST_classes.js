6a0d45170f016621a1942b478598b8d485207b46
lukelin1991 <lukelin1991@yahoo.com>
2020-08-17 13:20:40 -0400

    contains

--- BST/BST_classes.js
+++ BST/BST_classes.js
@@ -83,6 +83,21 @@ class BinarySearchTree {
         if(!found) return undefined
         return current
     }
+    contains(value){
+        if(this.root === null) return false
+        let current = this.root,
+            found = false
+        while(current && !found){
+            if(value < current.value){
+                current = current.left
+            } else if(value > current.value){
+                current = current.right
+            } else {
+                return true
+            }
+        }
+        return false
+    }
 }
 
 let tree = new BinarySearchTree();
