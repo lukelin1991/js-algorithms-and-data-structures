b7e5df45f476047458a59022f8960c6db8f1097e
lukelin1991 <lukelin1991@yahoo.com>
2020-08-20 17:43:22 -0400

    committing bubbleSort

---
+++ AE_Questions/sorting/bubbleSort.js
@@ -0,0 +1,30 @@
+/*
+Bubble Sort
+Write a function that takes in an array of integers and returns a sorted version of that array.
+Use the Bubble Sort algorithm to sort the array.
+
+Sample Input
+array = [8, 5, 2, 9, 5, 6, 3]
+
+Sample Output
+[2, 3, 5, 5, 6, 8, 9]
+*/
+
+function bubbleSort(array){
+    let noSwaps;
+	for(let i = array.length; i > 0;i--){
+        noSwaps = true;
+        for(let j = 0;j < i-1 ; j++){
+            if(array[j] > array[j+1]){
+                swap(array, j, j+1)
+                noSwaps = false;
+            }
+        }
+        if(noSwaps) break;
+    }
+    return array
+}
+
+const swap = (arr, i, j) => {
+    [arr[i], arr[j]] = [arr[j], arr[i]]
+}
