a977aa4d28a6db419fa8b8bedd1df3298cc2012f
lukelin1991 <lukelin1991@yahoo.com>
2020-08-24 18:50:37 -0400

    fibo

---
+++ AE_Questions/recursion/NthFibonacci.js
@@ -0,0 +1,25 @@
+/*
+Nth Fibonacci
+The Fibonacci sequence is defined as follows: first number of the sequence is 0, the 2nd number is 1, and the
+nth number is the sum of the (n - 1)th and (n - 2)th numbers.  Write a function that takes in an integer n
+and returns the nth Fibonacci number.
+
+Important note: the Fibonacci sequence is often defined with its first two numbers as F0 = 0 and F1 = 1.
+For the purpose of this question, the first Fibonacci number is F0; therefore, getNthFib(1) is equal to F0,
+getNthFib(2) is equal to F1, etc..
+
+Sample Input #1
+n = 2
+1 // 0, 1
+
+Sample Output #2
+n = 6
+5 // 0, 1, 1, 2, 3, 5
+*/
+
+function getNthFib(n) {
+    // Write your code here.
+    if(n === 2) return 1
+    if(n === 1) return 0
+    return getNthFib(n - 1) + getNthFib(n - 2)
+}
