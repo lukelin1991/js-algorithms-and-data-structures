b2c18eef7be5186096c6416809b33cf1b433b1a4
lukelin1991 <lukelin1991@yahoo.com>
2020-08-24 17:57:41 -0400

    heap sorting confusion

--- AE_Questions/sorting/heapSort.js
+++ AE_Questions/sorting/heapSort.js
@@ -10,6 +10,45 @@ Sample Output
 [2, 3, 5, 5, 6, 8, 9]
 */
 
-function heapSort(array){
+function heapSort(array) {
+    // Write your code here.
+    buildMaxHeap(array)
     
+    for(let i = array.length - 1; i > 0; i--){
+        swap(array, 0, i)
+        siftDown(array, 0, i - 1)
+    }
+    return array
+}
+
+function buildMaxHeap(array){
+        let firstParentIdx = Math.floor((array.length - 1)/ 2)
+        for(let idx = firstParentIdx; idx >= 0; idx--){
+            siftDown(array, idx, array.length - 1)
+        }
+}
+
+function siftDown(heap, currentIdx, endIdx){
+    let childOneIdx = currentIdx * 2 + 1
+    while(childOneIdx <= endIdx){
+        let childTwoIdx = currentIdx * 2 + 2 <= endIdx ? currentIdx * 2 + 2 : -1
+        let idxToSwap
+        if(childTwoIdx !== -1 && heap[childTwoIdx] > heap[childOneIdx]){
+            idxToSwap = childTwoIdx
+        } else {
+            idxToSwap = childOneIdx
+        }
+        
+        if(heap[idxToSwap] > heap[currentIdx]){
+            swap(heap, currentIdx, idxToSwap)
+            currentIdx = idxToSwap
+            childOneIdx = currentIdx * 2 + 1
+        } else {
+            return
+        }
+    }
+}
+
+const swap = (arr, i, j) => {
+    [arr[i], arr[j]] = [arr[j], arr[i]]
 }
