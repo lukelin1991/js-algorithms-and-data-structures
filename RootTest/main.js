0c72c47f11ac2557b2fb64645c1df4d9b181a899
lukelin1991 <lukelin1991@yahoo.com>
2020-09-14 12:52:14 -0400

    so far what i got

--- RootTest/main.js
+++ RootTest/main.js
@@ -1,39 +1,45 @@
-'use strict';
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+let fs = require('fs');
+fs.readFile('index.txt', 'utf8', function(err, data){
+    if(err) throw err;
+    const contentArray = data.split(/(\r\n|\n|\r)/gm)
+    parseArray(contentArray)
+})
 
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
-    });
-    
-    main();    
-});
-
-function readline() {
-    return inputString[currentLine++];
+function parseArray(arr){
+    arr.forEach(content => {
+        const action = content.split(" ")
+        console.log(action)
+    })
 }
+// parseArray(contentArr)
 
-//--------------------------------------------------------------------//
+// class Driver {
+//     constructor(name){
+//         this._name = name
+//         this._trips = Trips
+//     }
+// }
 
-function main() {
+// class Trips {
+//     constructor(startTime, stopTime, distance){
+//         this._start = startTime
+//         this._stop = stopTime
+//         this._distance = distance
+//     }
+//     get convertTime(){
+//         const startHr = self.startTime.split(":")[0]
+//         const startMin = self.startTime.split(":")[1]
+//         const stopHr = self.stopTime.split(":")[0]
+//         const stopMin = self.stopTime.split(":")[1]
+//         const totalTime = ((stopHr - startHr) * 60) + (stopMin - startMin)
+//         return totalTime
+//     }
+// }
 
-    const x = readline();
-    var line2 = readline(); 
-    
-    foo(x);
-    foo(line2);
-}
-
-function foo(x) {
-    process.stdout.write("hello: ");
-    console.log(x);  
-}
+/*
+Expected Output
+Lauren: 42 miles @ 34 mph
+Dan: 39 miles @ 47 mph
+Kumi: 0 miles
+*/
