6441156349e849d61163927c2bc0a6afa28407dd
lukelin1991 <lukelin1991@yahoo.com>
2020-09-22 13:14:01 -0400

    finished

--- RootTest/RootReadMe.md
+++ RootTest/RootReadMe.md
@@ -18,4 +18,30 @@ Kumi: 0 miles
 Step 1 - Creating a function that can initially read a .txt file.
   - I had contemplated on how to truly break this process down into smaller pieces
   - I had to google how to read a file, and break the file up line by line.
-  - 
+  - I had contemplated on how I can make this possible, and my mind went to creating a react application automatically
+  - although it would be nice, I know it would've taken a lot of time, utilizing react, and recoil as a state management
+  - so I opted for simplicity and wrote everything in a main.js file.
+
+Step 2 - What to do once the file is read and parsed into a string?
+  - This is where manipulation needs to happen, because I needed to set the information into 3 parts.
+  - first part is.. calculating total number of minutes taken for the drive.
+  - second part is how many total miles.
+  - third part is to calculate the avg miles per hour each person has driven based on Trip data.
+  - push all the data into an array into each individual's object key.
+
+Step 3 - What to do with object?
+  - Now that the format is in an object notation, it will be easier to manipulate.
+  - the only problem is that Dan traveled twice, Kumi traveled none.
+  - I have to further manipulate Dan's object value because he has 2 logs of travel.
+  - I concatenated the two arrays into 1, while also adjusting the strings into numerical value (int/float)
+  - By this time, my goal was to make sure that the way the data is displayed for each person in Key/value pair are all the same
+
+Step 4 - Once everything is lined up.
+  - the final step of the process was to ensure that each data is calculated properly.
+  - knowing that the array layout are all similar (except Kumi's). 
+  - I can then write out the output using backticks ` ` along with ${{objName} in order to write out the output
+  - since Kumi's array length will be different than everyone else's, I created an if/else statement to make sure 
+  that her output is unique compared to everybody else's.
+  - I created a function to wrap the final output in.
+
+By Luke Lin
--- RootTest/main.js
+++ RootTest/main.js
@@ -9,25 +9,37 @@ function parseArray(arr){
     let newObj = {}
     arr.forEach(content => {
         const action = content.split(" ")
+        // parsing input data into an object for easier use.
         if(action[0] === 'Driver'){
             newObj[action[1]] = []
         } else if(action[0] === 'Trip'){
-            // calculate time
-            const startTime = action[2]
-            const stopTime = action[3]
-            const startHr = startTime.split(":")[0]
-            const startMin = startTime.split(":")[1]
-            const stopHr = stopTime.split(":")[0]
-            const stopMin = stopTime.split(":")[1]
-            const minutes = ((stopHr - startHr) * 60) + (stopMin - startMin)
-
-            newObj[action[1]].push([minutes, action[4]])
+            // calculate time function, action[2] being the start time, action[3] being the end/stop time.
+            // action[4] is the number of miles.
+            newObj[action[1]].push([calculateTime(action[2], action[3]), action[4]])
         }
     })
+    manipulatingObj(newObj)
+}
 
-    let entries = Object.entries(newObj)
+// calculating total minutes from the inputs of the parsed content
+function calculateTime(startTime, stopTime){
+    const startHr = startTime.split(":")[0]
+    const startMin = startTime.split(":")[1]
+    const stopHr = stopTime.split(":")[0]
+    const stopMin = stopTime.split(":")[1]
+    const minutes = ((stopHr - startHr) * 60) + (stopMin - startMin)
 
+    return minutes
+}
+
+// manipulating the object to pinpoint our calculations (since Dan had traveled more than once, we have to put that data
+// together in order to optimize our object))
+function manipulatingObj(obj){
+    // split into names and logs for simplicity.
+    let entries = Object.entries(obj)
     for(let [name, logs] of entries){
+        // calculating total time vs total miles for avging out mph. also changing strings into int.
+        // this is to make sure dan's logs are concat'd and also to manipulte all the other strings into Int or float.
         if(logs.length > 1){
             let totalMiles = 0
             let totalTime = 0
@@ -45,13 +57,21 @@ function parseArray(arr){
         // get avg mph
         // total time = log[0]
         // total distance = log[1]
+        // total distance / (total time/60) for avg mph. or 0.
         let avgMph = (logs[1] / (logs[0]/60)) || 0
         logs.push(Math.round(avgMph))
-        
-        // final output
+        obj[name] = logs
+    }
+    finalOutput(obj)
+}
+
+// final output
+function finalOutput(changedObj){
+    let entries = Object.entries(changedObj)
+    for(let [name, logs] of entries){
         let miles = logs[1] || 0
         let mph = logs[2] || 0
-        if(logs.length === 0 ){
+        if(logs.length === 1 ){ // for kumi, we input a 0 as a placeholder for the miles. so logs.length is not 0, but 1.
             console.log(`${name}: ${miles} miles`)
         } else {
             console.log(`${name}: ${miles} miles @ ${mph} mph`)
@@ -59,8 +79,17 @@ function parseArray(arr){
     }
 }
 
+
 /*
-Expected Output
+Initial Input:
+Driver Dan
+Driver Lauren
+Driver Kumi
+Trip Dan 07:15 07:45 17.3
+Trip Dan 06:12 06:32 21.8
+Trip Lauren 12:01 13:16 42.0
+
+Expected Output:
 Lauren: 42 miles @ 34 mph
 Dan: 39 miles @ 47 mph
 Kumi: 0 miles
